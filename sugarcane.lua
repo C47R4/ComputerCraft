e7db1c02f9909e72e7b804a6fa4b7693f4c4a8b1
Önder <104029884+C47R4@users.noreply.github.com>
2025-03-15 03:32:06 +0300

    Update sugarcane.lua

--- sugarcane.lua
+++ sugarcane.lua
@@ -12,26 +12,26 @@ end
 
 function Program:Plant()
     redstone.setOutput("bottom", false)
-    os.sleep(10)
+    sleep(10)
     redstone.setOutput("bottom", true)
 end
 
 function Program:Harvest()
     redstone.setOutput("back", true)
-    os.sleep(10)
+    sleep(10)
     redstone.setOutput("back", false)
 end
 
 function Program:Cycle()
     self:Harvest()
-    os.sleep(10)
+    sleep(10)
     self:Plant()
 end
 
 function Program:Run()
     while true do
         self:Cycle
-        os.sleep(1)
+        sleep(1)
     end
 end
 
