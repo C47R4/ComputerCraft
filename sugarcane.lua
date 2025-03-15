27fbdf3d910647f88b45f2f487c27058c0c70272
C47R4 <sans5435sans54@gmail.com>
2025-03-15 03:41:33 +0300

    qwe

--- sugarcane.lua
+++ sugarcane.lua
@@ -12,26 +12,26 @@ end
 
 function Program:Plant()
     redstone.setOutput("bottom", false)
-    sleep(10)
+    os.sleep(10)
     redstone.setOutput("bottom", true)
 end
 
 function Program:Harvest()
     redstone.setOutput("back", true)
-    sleep(10)
+    os.sleep(10)
     redstone.setOutput("back", false)
 end
 
 function Program:Cycle()
     self:Harvest()
-    sleep(10)
+    os.sleep(10)
     self:Plant()
 end
 
 function Program:Run()
     while true do
-        self:Cycle
-        sleep(1)
+        self:Cycle()
+        os.sleep(1)
     end
 end
 
