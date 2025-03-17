043886c7ed54ced2ef93f2d3c0b803873fbc5e73
Önder <104029884+C47R4@users.noreply.github.com>
2025-03-17 15:11:54 +0300

    Update storage_server.lua

--- storage_server.lua
+++ storage_server.lua
@@ -1,4 +1,10 @@
 local RRL = peripheral.find("redstone_relay")
-local crop_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
 
-print(tostring(crop_0).."%")
+local storage_list = {}
+
+local crop_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
+table.insert(storage_list,{name = "Crop 0", value = crop_0})
+
+for _, storage in storage_list do
+  print(name .. ":" , tostring(crop_0).."%")
+end
