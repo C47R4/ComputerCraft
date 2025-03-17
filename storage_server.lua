ed7a98435c257fa91c7a165c25ea98ec87ec3dd9
Önder <104029884+C47R4@users.noreply.github.com>
2025-03-17 15:12:21 +0300

    Update storage_server.lua

--- storage_server.lua
+++ storage_server.lua
@@ -6,5 +6,5 @@ local crop_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
 table.insert(storage_list,{name = "Crop 0", value = crop_0})
 
 for _, storage in storage_list do
-  print(name .. ":" , tostring(crop_0).."%")
+  print(storage.name .. ":" , tostring(storage.value).."%")
 end
