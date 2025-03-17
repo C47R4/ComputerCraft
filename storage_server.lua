4a76014ef0600d68f80f9d46d5a9e3a9bf466da0
C47R4 <sans5435sans54@gmail.com>
2025-03-17 16:15:46 +0300

    asdasd

--- storage_server.lua
+++ storage_server.lua
@@ -1,4 +1,5 @@
 local RRL = peripheral.find("redstone_relay")
+rednet.open("top")
 
 local ServerLib = {}
 ServerLib.__index = ServerLib
@@ -34,7 +35,19 @@ function ServerLib:PrintInfo()
   print("[]--------------------[]")
 end
 
+function ServerLib:EventListener()
+  while true do
+    local id, msg, prot = rednet.receive()
+
+    if prot == "storage_data_ping" then
+      self:UpdateInfo()
+      rednet.send(id,self.storageList,prot)
+      print(id,"wanted to know about storage data.")
+    end
+  end
+end
+
 local Server = ServerLib.new()
 
-Server:UpdateInfo()
-Server:PrintInfo()
+print("Set and ready.")
+Server:EventListener()
