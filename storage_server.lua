7270904f60f73e874eed606ad2c920df1988bdc1
C47R4 <sans5435sans54@gmail.com>
2025-03-17 15:33:02 +0300

    cat cut

--- storage_server.lua
+++ storage_server.lua
@@ -14,3 +14,38 @@ table.insert(storage_list,{name = "[0]Prod", value = prod_0})
 for _, storage in pairs(storage_list) do
   print(storage.name .. ":" , tostring(storage.value).."%")
 end
+
+
+local ServerLib = {}
+ServerLib.__index = ServerLib
+
+function ServerLib.new()
+  local self = setmetatable({},ServerLib)
+
+  self.storageList = {}
+
+  return self
+end
+
+function ServerLib:UpdateInfo()
+  
+  self.storageList = {}
+
+  local crop_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
+  table.insert(self.storageList,{name = "[0]Crop", value = crop_0})
+  
+  local wood_0 = math.floor(RRL.getAnalogInput("front") / 15 * 100)
+  table.insert(self.storageList,{name = "[0]Wood", value = wood_0})
+  
+  local prod_0 = math.floor(RRL.getAnalogInput("left") / 15 * 100)
+  table.insert(self.storageList,{name = "[0]Prod", value = prod_0})
+
+end
+
+function ServerLib:PrintInfo()
+  print("[]---[Storage Info]---")
+  for _, storage in pairs(self.storageList) do
+    print(" |"..storage.name .. ":" , tostring(storage.value).."% | ")
+  end
+  print("[]--------------------")
+end
