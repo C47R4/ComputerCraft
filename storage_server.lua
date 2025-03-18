b75b1c29707643875ad2cb0df486dee9ba744b2a
Önder <104029884+C47R4@users.noreply.github.com>
2025-03-18 12:34:36 +0300

    Update storage_server.lua

--- storage_server.lua
+++ storage_server.lua
@@ -16,13 +16,13 @@ function ServerLib:UpdateInfo()
   
   self.storageList = {}
 
-  local crop_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
+  local crop_0 = math.floor(RRL.getAnalogInput("front") / 15 * 100)
   table.insert(self.storageList,{name = "[0]Crop", value = crop_0})
   
-  local wood_0 = math.floor(RRL.getAnalogInput("front") / 15 * 100)
+  local wood_0 = math.floor(RRL.getAnalogInput("right") / 15 * 100)
   table.insert(self.storageList,{name = "[0]Wood", value = wood_0})
   
-  local prod_0 = math.floor(RRL.getAnalogInput("left") / 15 * 100)
+  local prod_0 = math.floor(RRL.getAnalogInput("back") / 15 * 100)
   table.insert(self.storageList,{name = "[0]Prod", value = prod_0})
 
 end
